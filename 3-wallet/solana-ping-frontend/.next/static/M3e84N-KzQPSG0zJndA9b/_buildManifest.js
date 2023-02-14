a801e96c68b9dc78c21e9e20dc1a29283531e57a
iapposki <konark981999@gmail.com>
2023-02-14 16:47:12 +0530

    movie frontend

---
+++ 3-wallet/solana-ping-frontend/.next/static/M3e84N-KzQPSG0zJndA9b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3814de04921b33b2.css","static/chunks/pages/index-8be5946738463280.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
